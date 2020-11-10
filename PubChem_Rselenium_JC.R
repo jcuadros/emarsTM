9dea358f208a681fbeff08a6bf070968b49fedf0
Jordi Cuadros <jordi.cuadros@iqs.edu>
2020-11-10 22:24:20 +0100

    Removed debug code

--- PubChem_Rselenium_JC.R
+++ PubChem_Rselenium_JC.R
@@ -14,7 +14,6 @@ remDr$open(silent=T)
 remDr$navigate("view-source:https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/CCCCBr/JSON")
 html <- remDr$getPageSource()
 
-writeLines(html[[1]], "1.txt")
 xmlscript <- as.character(xml_find_first(read_html(html[[1]]),"//../pre/text()"))
 jsonprova <- fromJSON(xmlscript)
 
