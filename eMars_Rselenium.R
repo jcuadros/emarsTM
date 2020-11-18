e63e079fb6d218f6fc4eaff3f3e1b9478bf82e0f
Pol <poljarquev@iqs.edu>
2020-11-18 22:36:59 +0100

    Extreure JSON eMARS

--- eMars_Rselenium.R
+++ eMars_Rselenium.R
@@ -13,6 +13,7 @@ remDr <- rD[["client"]]
 remDr$open()
 remDr$navigate("https://emars.jrc.ec.europa.eu/en/emars/accident/search")
 
+#Extreure ID
 for (i in 1:216) {
   webp[i] <- remDr$getPageSource()
   num_char <- unlist(str_locate_all(webp[i], "PrintDetail"))
@@ -30,18 +31,17 @@ for (i in 1:216) {
 eMarsdf[1081,"ID"] <- "45163465-6cc7-db99-a456-542697aba916"
 eMarsdf[1082,"ID"] <- "aa4023ef-1ccf-3a28-ec6d-fb2fed44ed02"
 
-
+#Escriure URL
 for (i in 1:1082) {
   eMarsdf[i,"URL"] <- paste("view-source:https://emars.jrc.ec.europa.eu/api/JsonAccident/",eMarsdf[i,"ID"], sep="")
 }
-
 
 
 rD <- rsDriver(browser="firefox", port=4546L, verbose=F)
 remDr <- rD[["client"]]
 remDr$open(silent=T)
 
-
+#Extreure JSON + Descarregar a local
 for (i in 1:1082) {
   remDr$navigate(eMarsdf[i,"URL"])
   html <- remDr$getPageSource()
@@ -50,10 +50,12 @@ for (i in 1:1082) {
   write_json(reportwebJSON, paste(eMarsdf[i,"ID"],".json", sep = ""))
 }
 
+#Escriure rutes directori .json
 for (i in 1:1082) {
   eMarsdf[i,"Route"] <- paste("C:/Users/polja/Desktop/IQS/MEQ/TFM/emarsTM/JSON/",paste(eMarsdf[i,"ID"],".json",sep = ""),sep = "")
 }
 
+#Extreure títol Accident
 for (i in 1:1082) {
   reportJSON <- fromJSON(eMarsdf[i,"Route"])
   eMarsdf[i,"Title"]<- reportJSON$Data$AccidentVersion$Translation$Title
